ed74d90b217d4145da8508a6b13af59a7ba8915f
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2024-01-31 18:26:32 -0300

    added python support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
 		-- { import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,10 +12,17 @@ return {
 				"tailwindcss-language-server",
 				"typescript-language-server",
 				"css-lsp",
+				"black"
 			})
 		end,
 	},
-
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		opts = function(_, opts)
+				local nls = require("null-ls")
+				table.insert(opts.sources, nls.builtins.formatting.black)
+		end,
+	},
 	-- lsp servers
 	{
 		"neovim/nvim-lspconfig",
